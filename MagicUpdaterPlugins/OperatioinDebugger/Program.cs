60b08a36919f13653067c4a8a51e4dfc9c349cae
aleksey.kartunov@sela.ru <aleksey.kartunov@sela.ru>
2017-08-14 17:39:10 +0300

    CouchDBExecuteCommandAction добавлено выполнение произвольной команды с параметром

--- MagicUpdaterPlugins/OperatioinDebugger/Program.cs
+++ MagicUpdaterPlugins/OperatioinDebugger/Program.cs
@@ -1,66 +1,61 @@
 ﻿using MagicUpdaterCommon.Abstract;
-using System;
-using MagicUpdater.Core;
-using MagicUpdaterCommon.Common;
-using MagicUpdaterCommon.SettingsTools;
-using System.Diagnostics;
-using System.Threading;
-using CouchDBDeployerPlugin;
 using MagicUpdaterCommon.Helpers;
+using System;
 
 namespace OperatioinDebugger
 {
-	class Program
-	{
-		//Наследуемся от операции, которую хотим дебажить. (без атрибутов)
-		//public class OperationTest : TestAttrPluginOperation.TestAttrPluginOperation
-		//{
-		//	public OperationTest(int? operationId, string attrsJson) : base(operationId, attrsJson)
-		//	{
-		//	}
+    class Program
+    {
+        //Наследуемся от операции, которую хотим дебажить. (без атрибутов)
 
-		//	protected override bool isPluginTestingMode => true;
-		//}
+        public class OperationTest : CouchDbExecuteCommandPlugin.CouchDbExecuteCommandPlugin
+        {
+            public OperationTest(int? operationId, string attrsJson) : base(operationId, attrsJson)
+            {
+            }
 
-		//Наследуемся от операции, которую хотим дебажить. (с атрибутами)
-		//public class OperationTest : TestAttrPluginOperation.TestAttrPluginOperation
-		//{			
-		//	public OperationTest(int? operationId, string attrsJson) : base(operationId, attrsJson)
-		//	{
-		//	}
+            protected override bool isPluginTestingMode => true;
+        }
 
-		//	protected override bool isPluginTestingMode => true;
-		//}
+        //Наследуемся от операции, которую хотим дебажить. (с атрибутами)
+        //public class OperationTest : TestAttrPluginOperation.TestAttrPluginOperation
+        //{			
+        //	public OperationTest(int? operationId, string attrsJson) : base(operationId, attrsJson)
+        //	{
+        //	}
 
-		//С приложением настроек
-		private const bool WITH_SETTINGS = true;
+        //	protected override bool isPluginTestingMode => true;
+        //}
 
-		static void Main(string[] args)
-		{
-			//var muCore = new MuCore(true);
+        //С приложением настроек
+        private const bool WITH_SETTINGS = true;
 
-			//if (WITH_SETTINGS)
-			//{
-			//	Process.Start(@"..\..\..\..\MagicUpdater\bin\Debug\MagicUpdaterSettings.exe");
-			//	Thread.Sleep(3000);
-			//}
+        static void Main(string[] args)
+        {
+            //var muCore = new MuCore(true);
 
-			//Операция без атрибутов
-			//Operation operation = (new OperationTest(null)) as Operation;
+            //if (WITH_SETTINGS)
+            //{
+            //    Process.Start(@"..\..\..\..\MagicUpdater\bin\Debug\MagicUpdaterSettings.exe");
+            //    Thread.Sleep(3000);
+            //}
 
-			//Операция с атрибутами
-			//Operation operation = (new OperationTest(null, NewtonJson.GetJsonFromModel(new TestAttrPluginOperation.OperationAttributes()))) as OperationWithAttr<TestAttrPluginOperation.OperationAttributes>;
+            //Операция без атрибутов
+            //Operation operation = (new OperationTest(null)) as Operation;
 
-			//operation.Run();
+            //Операция с атрибутами
+            Operation operation = (new OperationTest(null, NewtonJson.GetJsonFromModel(new CouchDbExecuteCommandPlugin.CouchDbExecuteCommandPluginAttr()))) as OperationWithAttr<CouchDbExecuteCommandPlugin.CouchDbExecuteCommandPluginAttr>;
 
-			//Console.WriteLine("Нажмите любую клавишу...");
-			//Console.ReadKey();
+            operation.Run();
 
-			//if (WITH_SETTINGS)
-			//{
-			//	Tools.KillAllProcessByname(MainSettings.Constants.MAGIC_UPDATER_SETTINGS, true);
-			//}
-			//muCore.Dispose();
-		}
-	}
+            Console.WriteLine("Нажмите любую клавишу...");
+            Console.ReadKey();
+
+            //if (WITH_SETTINGS)
+            //{
+            //    Tools.KillAllProcessByname(MainSettings.Constants.MAGIC_UPDATER_SETTINGS, true);
+            //}
+            //muCore.Dispose();
+        }
+    }
 }
